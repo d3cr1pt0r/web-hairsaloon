a4c04bd436f2ba77e260d939078ca3557f22e644
d3cr1pt0r <klemen.brajkovic@gmail.com>
2014-10-06 00:00:41 +0200

    ...

--- app/views/backend/parts/header.blade.php
+++ app/views/backend/parts/header.blade.php
@@ -7,7 +7,11 @@
 {{ HTML::script('assets/js/jquery-2.1.1.min.js') }}
 {{ HTML::script('assets/js/less.min.js') }}
 {{ HTML::script('assets/js/bootstrap.min.js') }}
+{{ HTML::script('assets/js/jquery.tablesorter.min.js') }}
+{{ HTML::script('assets/js/init.js') }}
+{{ HTML::script('assets/js/functions.js') }}
 
 <script>
 var toggleCheckboxAction = "{{ action('ActionController@postToggleValue') }}";
+$(".table").tablesorter();
 </script>
